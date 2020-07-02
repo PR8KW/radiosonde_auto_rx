835cc6563d660b2e0a3590c98bb93fdf356f3c18
PR8KW <48393217+PR8KW@users.noreply.github.com>
2020-07-02 12:21:51 -0300

    Update telegram.py

--- auto_rx/autorx/telegram.py
+++ auto_rx/autorx/telegram.py
@@ -125,7 +125,7 @@ class TelegramNotification(object):
                     urllib.urlencode({
                     "chat_id": self.chat_id,
                     "text": msg,
-		    "disable_web_page_preview": True,
+                    "disable_web_page_preview": True,
                     }), { "Content-type": "application/x-www-form-urlencoded" })
                 conn.getresponse()
 
@@ -175,7 +175,7 @@ class TelegramNotification(object):
                             urllib.urlencode({
                             "chat_id": self.chat_id,
                             "text": msg,
-			    "disable_web_page_preview": True,
+                            "disable_web_page_preview": True,
                             }), {"Content-type": "application/x-www-form-urlencoded"})
                         conn.getresponse()
 
